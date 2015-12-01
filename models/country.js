1980b371763927ed63c019690ff1810d77b8ef68
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-11-30 18:55:54 -0800

    Style fix

--- models/country.js
+++ models/country.js
@@ -1,16 +1,15 @@
 var mongoose = require('mongoose');
 
 var countrySchema = new mongoose.Schema({
-	name: {type: String, required: true},
-	places: String,
-	durationDays: {type: Number, validate: {
-		validator: function(value) {
-			Number.max = 365;
-			return value == 1;
-			}, message: "Please, enter correct duration"
-		}
-	},
-	description: String
+  name: {type: String, required: true},
+  places: String,
+  durationDays: {type: Number, validate: {
+    validator: function(value) {
+      Number.max = 365;
+      return value == 1;
+    }, message: "Please, enter correct duration" }
+  },
+  description: String
 });
 
 module.exports = mongoose.model('Country', countrySchema);
